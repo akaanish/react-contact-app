704300e04ff3d5a9992778816ffddbec9d5a3b39
anish.kumar1 <anish.kumar@plavaga.in>
2018-11-18 12:57:20 +0530

    Updates

---
+++ precache-manifest.707eb59c966c53f8d1518033152ab96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dd5175ecd4e8038b39d",
+    "url": "/react-contact-app/static/js/runtime~main.6dd5175e.js"
+  },
+  {
+    "revision": "c3ade3a1b4604510320c",
+    "url": "/react-contact-app/static/js/main.c3ade3a1.chunk.js"
+  },
+  {
+    "revision": "33a1b546f167234db124",
+    "url": "/react-contact-app/static/js/1.33a1b546.chunk.js"
+  },
+  {
+    "revision": "c3ade3a1b4604510320c",
+    "url": "/react-contact-app/static/css/main.3b74b38f.chunk.css"
+  },
+  {
+    "revision": "33a1b546f167234db124",
+    "url": "/react-contact-app/static/css/1.00525792.chunk.css"
+  },
+  {
+    "revision": "d8b4ff997a9faeaeb7bc136033b4c441",
+    "url": "/react-contact-app/index.html"
+  }
+];
